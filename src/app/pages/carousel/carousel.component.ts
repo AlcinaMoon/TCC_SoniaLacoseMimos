c28d928e7d0e52e37d307e22b3f155075790c5b3
AlcinaMoon <alcinamoon@gmail.com>
2025-11-13 16:36:39 -0300

    prototipo de adição do carrossel

---
+++ src/app/pages/carousel/carousel.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent {
+  images = [
+    'assets/img1.jpg',
+    'assets/img2.jpg',
+    'assets/img3.jpg'
+  ];
+
+  currentIndex = 0;
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+}
