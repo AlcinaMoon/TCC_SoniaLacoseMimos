6a377a89f28cbf9fc954a980992e1158e359e942
AlcinaMoon <alcinamoon@gmail.com>
2025-11-18 13:51:45 -0300

    mudança de paginas pelo header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RegrasComponent } from './pages/regras/regras.component';
+import { ShopComponent } from './pages/shop/shop.component';
+
 
 export const routes: Routes = [
   {
@@ -7,6 +12,21 @@ export const routes: Routes = [
     component: HomeComponent,
   }
   ,
+  {
+    path: 'Sobre', component: AboutComponent
+  }
+  ,
+  {
+    path: 'Contato', component: ContactComponent
+  }
+  ,
+  {
+    path: 'Regras', component: RegrasComponent
+  }
+  ,
+  {
+    path: 'Loja', component: ShopComponent
+  }
   
 ];
 
