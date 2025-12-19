29a5127564bba128289754e9514f73877bf028be
AlcinaMoon <alcinamoon@gmail.com>
2025-12-18 22:51:51 -0300

    LGPD nas paginas

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; 
 
 @Component({
-selector: 'app-sobre',
-standalone: true,
-templateUrl: './about.component.html',
-styleUrls: ['./about.component.css']
+  selector: 'app-sobre',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {}
+export class AboutComponent {
+  // Define qual seção está visível.
+  secaoAtiva: 'sobre' | 'termos' | 'privacidade' = 'sobre';
+
+  // trocar a seção 
+  mudarSecao(secao: 'sobre' | 'termos' | 'privacidade') {
+    this.secaoAtiva = secao;
+    // Rola 
+    document.querySelector('.card-conteudo')?.scrollIntoView({ behavior: 'smooth' });
+  }
+}
